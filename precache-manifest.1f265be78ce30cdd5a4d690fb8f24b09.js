a3eac3240370f065bee0f0e6ade26191765c402a
Dede Herman <dedehermanvpay@gmail.com>
2021-01-23 13:49:38 +0700

    Updates

---
+++ precache-manifest.1f265be78ce30cdd5a4d690fb8f24b09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "999775f748a9ba9263990dbb399c115d",
+    "url": "/aplikasi-taskku/index.html"
+  },
+  {
+    "revision": "8b4e44096ea8c4f1a0b6",
+    "url": "/aplikasi-taskku/static/css/main.d1b730c6.chunk.css"
+  },
+  {
+    "revision": "ee29ad8bf07d2722268b",
+    "url": "/aplikasi-taskku/static/js/2.fa1a7d8f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/aplikasi-taskku/static/js/2.fa1a7d8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b4e44096ea8c4f1a0b6",
+    "url": "/aplikasi-taskku/static/js/main.66144e4a.chunk.js"
+  },
+  {
+    "revision": "f983e48a738abada73aa",
+    "url": "/aplikasi-taskku/static/js/runtime-main.bc924faa.js"
+  }
+]);
